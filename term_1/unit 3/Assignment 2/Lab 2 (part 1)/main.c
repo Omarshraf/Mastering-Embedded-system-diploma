cc119feda095006e341199c8015c8b7050725e80
Omar Ashraf <116748127+Omarshraf@users.noreply.github.com>
2023-05-12 15:57:56 +0300

    Lab 2 (part 1)

---
+++ term_1/unit 3/Assignment 2/Lab 2 (part 1)/main.c
@@ -0,0 +1,32 @@
+typedef volatile unsigned int  vuint32_t;
+#include <stdint.h>
+#define RCC_BASE 	0x40021000
+#define GPIOA_BASE  0x40010800
+#define RCC_APB2ENR *((volatile uint32_t *)( RCC_BASE +0x18))
+#define GPIOA_CRH   *(volatile uint32_t *)(GPIOA_BASE+0X04)
+#define GPIOA_ODR   *(volatile uint32_t *)(GPIOA_BASE+0X0C)
+
+typedef union {
+	vuint32_t all_fields;
+	struct 
+	{
+		vuint32_t reserved:13;
+		vuint32_t P_13:1;
+	}Pin;
+}R_ODR_t;
+
+volatile R_ODR_t* R_ODR= (volatile R_ODR_t*)((GPIOA_BASE+0X0C));
+int main(void)
+{
+	RCC_APB2ENR|=1<<2;
+	GPIOA_CRH&=0xFF0FFFFF;
+	GPIOA_CRH|=0x00200000;
+	while(1){
+		R_ODR ->Pin.P_13=1;
+		for (int i = 0; i < 5000; ++i);
+		R_ODR ->Pin.P_13=0;
+		for (int i = 0; i < 5000; ++i);
+	}
+	return 0;
+}
+
